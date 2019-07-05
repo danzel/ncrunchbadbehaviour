68362394b5558ff28813084b18eaa7543f489c63
dave <dave@torutek.com>
2019-07-05 16:16:47 +1200

    Code and images

---
+++ TestTest/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using Xunit;
+
+namespace TestTest
+{
+	public class Class1
+	{
+		public static IEnumerable<object[]> RandomSeeds()
+		{
+			return Enumerable.Range(0, 100).Select(n => new object[] { n });
+		}
+
+		[Theory]
+		[MemberData(nameof(RandomSeeds))]
+		public void Test(int randomSeed)
+		{
+			var rand = new Random(randomSeed);
+
+			//Test code normally goes here
+			//throw new Exception();
+			Thread.Sleep(TimeSpan.FromSeconds(5));
+			Console.WriteLine("asd23456");
+		}
+	}
+}
